46e463d71b0f8057bd2c12a6edc27e6236911085
Neylan-Body <neylan2011@gmail.com>
2021-08-23 10:26:13 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada<>();
+
+        minhaListaEncadeada.add("teste1");
+        minhaListaEncadeada.add("teste2");
+        minhaListaEncadeada.add("teste3");
+        minhaListaEncadeada.add("teste4");
+        minhaListaEncadeada.add("teste5");
+        System.out.println(minhaListaEncadeada.get(4));
+        System.out.println(minhaListaEncadeada);
+        System.out.println(minhaListaEncadeada.remove(3));
+        System.out.println(minhaListaEncadeada);
+
+    }
+}
